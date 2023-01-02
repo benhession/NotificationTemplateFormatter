6c631b461e7b9b674cf8eb530540c1936dd831fc
Ben Hession <bshession@gmail.com>
2023-01-02 14:05:43 +0000

    initial commit

---
+++ src/main/java/com/benhession/notificationformatterservice/NotificationFormatterServiceApplication.java
@@ -0,0 +1,12 @@
+package com.benhession.notificationformatterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationFormatterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationFormatterServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/com/benhession/notificationformatterservice/NotificationFormatterServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.benhession.notificationformatterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationFormatterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
